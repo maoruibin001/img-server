20a5041d59f8dbc8f6e63d8291c1fd3fb05abdf7
毛瑞彬 <www.maoruibin@163.com>
2019-08-22 11:38:09 +0800

    chore: 提交

---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Res struct {
+	Status int `json:"string"`
+	Url string `json:"url"`
+	Result string `json:"result"`
+}
+
+const maxUploadSize = 20 * 1024 * 1024 // 20 mb
+const uploadPath = "./files"
+const host = "http://localhost:3433/"
+
+func main() {
+	http.HandleFunc("/upload", uploadFileHandler())
+
+	fs := http.FileServer(http.Dir(uploadPath))
+	http.Handle("/files/", http.StripPrefix("/files", fs))
+
+	log.Print("Server started on localhost:3433, use /upload for uploading files and /images/{fileName} for downloading")
+	log.Fatal(http.ListenAndServe(":3433", nil))
+}
+
+func uploadFileHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// validate file size
+		r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
+		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
+			renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
+			return
+		}
+
+		file, _, err := r.FormFile("uploadFile")
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+		fileBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
+
+		// check file type, detectcontenttype only needs the first 512 bytes
+		fileType := http.DetectContentType(fileBytes)
+		switch fileType {
+		case "image/jpeg", "image/jpg":
+		case "image/gif", "image/png":
+		case "application/pdf":
+			break
+		default:
+			renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
+			return
+		}
+		fileExt := strings.Split(fileType, "/")[1]
+		fmt.Println("fileExt", fileExt)
+		fileName := randToken(12)
+		fileEndings, err := mime.ExtensionsByType(fileType)
+		fmt.Println("fileEndings", fileEndings)
+		if err != nil {
+			renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
+			return
+		}
+		newPath := filepath.Join(uploadPath, fileName + "." + fileExt)
+		fmt.Printf("FileType: %s, File: %s\n", fileType, newPath)
+
+		// write file
+		newFile, err := os.Create(newPath)
+		if err != nil {
+			renderError(w, "CANT_WRITE_FILE1", http.StatusInternalServerError)
+			return
+		}
+		defer newFile.Close() // idempotent, okay to call twice
+		if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
+			renderError(w, "CANT_WRITE_FILE2", http.StatusInternalServerError)
+			return
+		}
+		res := &Res {
+			Result: "success",
+			Status: 200,
+			Url: host + newPath,
+		}
+		resStr, err := json.Marshal(res)
+		w.Write([]byte(resStr))
+	})
+}
+
+func renderError(w http.ResponseWriter, message string, statusCode int) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(message))
+}
+
+func randToken(len int) string {
+	b := make([]byte, len)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
